686f8c6a65ca4ebea8dc92d8665720e56c65bd67
Munir <emammunir@gmail.com>
2025-10-16 19:49:52 -0700

    Add built files for GitHub Pages

---
+++ out/_next/static/chunks/2acfbec923bf691c.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/8c0bf8bdbeb6e59a.js",
+  "static/chunks/596448d7701c94a1.js",
+  "static/chunks/turbopack-b01720c27f0dee19.js"
+])
---
+++ out/_next/static/chunks/433d2513fe9626f8.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/4cf5893b5eb81be2.js",
+  "static/chunks/596448d7701c94a1.js",
+  "static/chunks/turbopack-0ec457cff0a0d63c.js"
+])
